9e01bb25b5eacd82605ace7fe7dea8a815f01cc7
NirajGujarathi <32728058+NirajGujarathi@users.noreply.github.com>
2021-07-28 00:46:25 +0530

    Create 31_Next Permutation.java

---
+++ 31_Next Permutation.java
@@ -0,0 +1,55 @@
+// c++ single line next_permutation(nums.begin(),nums.end());
+
+// java 
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
+        if(i>=0){
+            int j=nums.length-1;
+            while(j>i && nums[j]<=nums[i]) j--;
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    
+    private void swap(int []nums,int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    
+    private void reverse(int []nums,int i,int j){
+        while(i<j) swap(nums,i++,j--);
+    }
+}
+
+/*
+ Question 
+ 
+ Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+
+*/
